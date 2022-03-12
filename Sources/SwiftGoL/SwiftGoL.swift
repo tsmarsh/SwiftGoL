9292c1438e4fae3349ae74aa880565926e6a441c
Tom Marsh <ts.marsh@gmail.com>
2022-03-12 15:46:45 -0500

    Initial Commit

---
+++ Sources/SwiftGoL/SwiftGoL.swift
@@ -0,0 +1,6 @@
+public struct SwiftGoL {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SwiftGoLTests/SwiftGoLTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftGoL
+
+final class SwiftGoLTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftGoL().text, "Hello, World!")
+    }
+}
