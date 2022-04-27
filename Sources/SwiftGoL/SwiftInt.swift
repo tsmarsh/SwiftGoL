e6adb6b60276d788d657b499d661383e06cdf109
Tom Marsh <ts.marsh@gmail.com>
2022-04-27 01:05:18 -0400

    working on next
    '

--- Sources/SwiftGoL/SwiftInt.swift
+++ Sources/SwiftGoL/SwiftInt.swift
@@ -5,6 +5,8 @@
 //  Created by Thomas Marsh on 4/23/22.
 //
 
+import Security
+
 public class Game {
     public var width: Int = 0
     
@@ -27,22 +29,59 @@ public class Game {
 public struct SwiftGoLInt : Gol{
     public typealias T = Game
     
-    private struct Location {
+    static let mask: UInt64 = 7 + (5 << 8) + (7 << 16)
+    
+    struct Location : Equatable{
         var bucket: Int
         var bit: Int
     }
     
-    private static func toLocation(world: Game, coord: Coord) -> Location {
-        let index = coord.x + coord.y * world.width
-        return Location(bucket: index / 64, bit: index % 64)
+    static func toIndex(width: Int, coord: Coord) -> Int {
+        return coord.x + coord.y * width
+    }
+    
+    static func printModule(module: UInt64) {
+        var i = 0
+
+        var row = ""
+        for _ in 0...7 {
+            for _ in 0...7 {
+                let mask: UInt64 = 1 << i
+                let value = (module & mask) >= 1
+                
+                i += 1
+                
+                row.append(contentsOf: value ? "X" : ".")
+            }
+            row.append(contentsOf: "\n")
+        }
+        print(row)
+    }
+    
+    static func toLocation(world: Game, coord: Coord) -> Location {
+        let module_x: Int = coord.x / 6
+        let module_y: Int = coord.y / 6
+        
+        let module_index = toIndex(width: world.width, coord: Coord(module_x, module_y))
+        
+        let cell_x: Int = (coord.x % 6) + 1
+        let cell_y: Int = (coord.y % 6) + 1
+        
+        let bin_index = toIndex(width: 8, coord: Coord(cell_x, cell_y))
+        
+        return Location(bucket: module_index, bit: bin_index)
     }
     
     public static func isAlive(world: Game, coords: Coord) -> Bool {
         let loc = toLocation(world: world, coord: coords)
         
         let bucket = world.world[ loc.bucket ]
-        let mask: UInt64 = 1 << (loc.bit - 1)
-        return bucket & mask == 1
+        let mask: UInt64 = 1 << loc.bit
+        
+        print("isAlive Bucket", bucket)
+        printModule(module: bucket)
+        
+        return bucket & mask >= 1
     }
     
     
@@ -51,17 +90,51 @@ public struct SwiftGoLInt : Gol{
         let loc = toLocation(world: world, coord: coords)
         
         let bucket = world.world[ loc.bucket ]
-        let mask: UInt64 = 1 << (loc.bit - 1)
+        let mask: UInt64 = 1 << loc.bit
+        
+        print("Bringing to life: ", loc.bucket, loc.bit, bucket, mask)
         world.world[ loc.bucket ] = bucket | mask
         
         return world
     }
     
     public static func kill(world: Game, coords: Coord) -> Game {
+        let loc = toLocation(world: world, coord: coords)
+        
+        let bucket = world.world[ loc.bucket ]
+        let mask: UInt64 = 1 << loc.bit
+        world.world[ loc.bucket ] = bucket & ~mask
+        
         return world
     }
     
+    public static func countLivingNeighbours(world: Game, coords: Coord) -> UInt8 {
+        let loc = toLocation(world: world, coord: coords)
+        let offset = (8 * coords.y) + coords.x
+        
+        let module = world.world[loc.bucket]
+        let fence = (mask << offset)
+        
+        let neighbours = module & fence
+        
+        return UInt8(neighbours.nonzeroBitCount)
+    }
+    
     public static func next(world: Game) -> Game {
+//        for module in world.world {
+//            var i = 0
+//            for _ in 0...7 {
+//                for _ in 0...7 {
+//                    let mask: UInt64 = 1 << i
+//                    let value = (module & mask) >= 1
+//                    
+//                    i += 1
+//                    
+//                    row.append(contentsOf: value ? "X" : ".")
+//                }
+//                row.append(contentsOf: "\n")
+//            }
+//        }
         return world
     }
 }
--- Tests/SwiftGoLTests/SwiftGoLIntTest.swift
+++ Tests/SwiftGoLTests/SwiftGoLIntTest.swift
@@ -16,12 +16,30 @@ class SwiftGolIntTest: XCTestCase {
         XCTAssertFalse(SwiftGoLInt.isAlive(world: world, coords: Coord(0, 0)))
     }
     
+    func testLocationCalculator() {
+        let world = Game(moduleCount: 2)
+        
+        let test1 = SwiftGoLInt.toLocation(world: world, coord: Coord(3,4))
+        let test2 = SwiftGoLInt.toLocation(world: world, coord: Coord(8,0))
+        let test3 = SwiftGoLInt.toLocation(world: world, coord: Coord(7,11))
+        
+        XCTAssertTrue(SwiftGoLInt.Location(bucket: 0, bit: 44) == test1)
+        XCTAssertTrue(SwiftGoLInt.Location(bucket: 1, bit: 11) == test2)
+        XCTAssertTrue(SwiftGoLInt.Location(bucket: 3, bit: 50) == test3)
+    }
+    
     func testACellCanComeToLife() {
         var world = Game(moduleCount: 1)
         
         XCTAssertFalse(SwiftGoLInt.isAlive(world: world, coords: Coord(0, 0)))
         world = SwiftGoLInt.bringToLife(world: world, coords: Coord(0, 0))
+        world = SwiftGoLInt.bringToLife(world: world, coords: Coord(0, 1))
+        world = SwiftGoLInt.bringToLife(world: world, coords: Coord(1, 0))
+        
         XCTAssertTrue(SwiftGoLInt.isAlive(world: world, coords:  Coord(0, 0)))
+        XCTAssertTrue(SwiftGoLInt.isAlive(world: world, coords:  Coord(0, 1)))
+        XCTAssertTrue(SwiftGoLInt.isAlive(world: world, coords:  Coord(1, 0)))
+        XCTAssertFalse(SwiftGoLInt.isAlive(world: world, coords:  Coord(1, 1)))
     }
     
     func testACellCanDie() {
@@ -41,13 +59,13 @@ class SwiftGolIntTest: XCTestCase {
 //        XCTAssertEqual(expected, Set(SwiftGoLInt.getNeighbours(coords: Coord(1, 1))))
 //    }
     
-//    func testCanCountLivingNeighbours(){
-//        let world = Game(moduleCount: 1, state: [Coord(0, 1),Coord(1, 0), Coord(1, 1), Coord(1, 2),Coord(2, 1)])
-//
-//        XCTAssertEqual(4, SwiftGoLInt.countLivingNeighbours(world: world, coords: Coord(1, 1)))
-//        XCTAssertEqual(3, SwiftGoLInt.countLivingNeighbours(world: world, coords: Coord(0, 0)))
-//        XCTAssertEqual(1, SwiftGoLInt.countLivingNeighbours(world: world, coords: Coord(3, 1)))
-//    }
+    func testCanCountLivingNeighbours(){
+        let world = Game(moduleCount: 1, state: [Coord(0, 1),Coord(1, 0), Coord(1, 1), Coord(1, 2),Coord(2, 1)])
+
+        XCTAssertEqual(4, SwiftGoLInt.countLivingNeighbours(world: world, coords: Coord(1, 1)))
+        XCTAssertEqual(3, SwiftGoLInt.countLivingNeighbours(world: world, coords: Coord(0, 0)))
+        XCTAssertEqual(1, SwiftGoLInt.countLivingNeighbours(world: world, coords: Coord(3, 1)))
+    }
     
     func testACellWithTwoLivingNeighboursComesToLife() {
         let world = Game(moduleCount: 1, state: [Coord(0, 0), Coord(1, 0), Coord(1, 2)])
